c3c09c86c7c6c195c0012110fd29dab80f86a169
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2023-11-16 12:44:45 +0545

    Added cors to package and used it on index

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors';
 import { port } from './config/sconfig.js'
 
 const app = express()
+
+//cors to let browser access the hosted backend
+app.use(cors({
+    origin : '*',
+    methods : 'GET, POST, PUT, PATCH, DELETE',
+    credentials : true
+}));
 
 app.use(bodyParser.json())
 
