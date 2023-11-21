c58bb8a0f57610c3853d728aac06c4f9cede7134
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2023-11-21 17:13:50 +0545

    Added model code to review model

---
+++ models/reviews.js
@@ -0,0 +1,33 @@
+import Sequelize, { DataTypes } from "sequelize";
+import { sequelize } from "../connectDb/dbPostgres.js";
+import { User } from "./user.js";
+
+export const Reviews = sequelize.define(
+  "Reviews",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  { freezeTableName: true }
+);
+
+Reviews.belongsTo(User, {
+  foreignKey: "user",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE",
+});
