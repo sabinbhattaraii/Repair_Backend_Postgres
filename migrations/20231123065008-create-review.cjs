9c381cbcc85dd7de6cb8e8b3460caf92281fcf2e
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2023-11-23 16:28:48 +0545

    Added created and updated in migration of review

--- migrations/20231123065008-create-review.cjs
+++ migrations/20231123065008-create-review.cjs
@@ -31,6 +31,16 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
       },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
     });
   },
 
