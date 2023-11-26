1b0891bd8cef2d02e673658eeab7c99b61a5e861
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2023-11-26 11:36:28 +0545

    Added new controller file for category service and added it to index

---
+++ controller/categoryController.js
@@ -0,0 +1,65 @@
+import { HttpStatus } from "../constant/constant.js";
+import catchAsyncError from "../middleware/catchAsyncError.js";
+import { sendErrResponseByMsg } from "../middleware/errorMiddleware.js";
+import successResponseData from "../helper/successResponseData.js";
+import { categoryService } from "../service/index.js";
+
+export const createCategory = catchAsyncError(async(req,res) => {
+    let { courseType } = req.body;
+    if(!courseType) {
+        sendErrResponseByMsg(res,'Course Type is required',HttpStatus.BAD_REQUEST)
+    }
+
+    let data = await categoryService.createCategoryService(req.body,res);
+
+    successResponseData({
+        res:res,
+        message : "Category is created successfully",
+        status : HttpStatus.CREATED,
+        data
+    })
+})
+
+export const getSpecifiedCategory = catchAsyncError(async(req,res) => {
+    let id = req.params.id
+    let data = await categoryService.getSpecifiedCategoryService(id)
+    if(!data) {
+        sendErrResponseByMsg(res,"Category Not Found",HttpStatus.NOT_FOUND)
+    }
+    successResponseData({
+        res : res,
+        message : 'Specified Category Found',
+        status : HttpStatus.OK,
+        data
+    })
+})
+
+export const getAllCategory = catchAsyncError(async(req,res,next) => {
+    let find = {}
+    req.find = find
+    req.service = await categoryService.getAllCategoryService
+    next()
+})
+
+export const updateCategory = catchAsyncError(async(req,res,next) => {
+    let body = req.body
+    let id = req.params.id
+    let data = await categoryService.updateCategoryService({data : body,id});
+    successResponseData({
+        res : res,
+        message : `Category with this ${id} updated successfully`,
+        statusCode : HttpStatus.OK,
+        data
+    })
+})
+
+export const deleteCategory = catchAsyncError(async(req,res,next) => {
+    let id = req.params.id
+    let data = await categoryService.deleteCategoryService(id);
+    successResponseData({
+        res : res,
+        message : `Category with this ${id} deleted sucessfully`,
+        statusCode : HttpStatus.OK,
+        data
+    })
+})
