72197e136612173252842a4c007bf3037aebf8d7
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2023-11-26 12:38:32 +0545

    Added routes in category router

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,4 +4,15 @@ import { sortFilterPagination } from "../middleware/sortSelectPage.js";
 
 const categoryRouter = Router()
 
+categoryRouter
+    .route('/')
+    .post(categoryController.createCategory)
+    .get(categoryController.getAllCategory,sortFilterPagination)
+
+categoryRouter
+    .route('/:id')
+    .get(categoryController.getSpecifiedCategory)
+    .patch(categoryController.updateCategory)
+    .delete(categoryController.deleteCategory)
+
 export default categoryRouter
